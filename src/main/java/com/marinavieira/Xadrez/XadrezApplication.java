0c2079fc68f84c4788acf46e9c9a3f8d851d7a68
marigv95 <mama_guima04@hotmail.com>
2023-12-12 17:02:12 -0300

    Preparing the repository

---
+++ src/main/java/com/marinavieira/Xadrez/XadrezApplication.java
@@ -0,0 +1,13 @@
+package com.marinavieira.Xadrez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XadrezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XadrezApplication.class, args);
+	}
+
+}

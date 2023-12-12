d9c03c2239aa8fdbd1109870031ef6790ebfcaff
marigv95 <mama_guima04@hotmail.com>
2023-12-12 17:17:15 -0300

    First Class: Position

--- src/main/java/com/marinavieira/Xadrez/XadrezApplication.java
+++ src/main/java/com/marinavieira/Xadrez/XadrezApplication.java
@@ -1,13 +1,19 @@
 package com.marinavieira.Xadrez;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.marinavieira.boardgame.Position;
 
 @SpringBootApplication
 public class XadrezApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XadrezApplication.class, args);
+	
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+	
+	
 	}
 
 }

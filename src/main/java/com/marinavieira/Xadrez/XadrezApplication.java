1ebdb073ef90bd5dee86611876b847942c704560
marigv95 <mama_guima04@hotmail.com>
2023-12-14 17:11:22 -0300

    Handling exceptions and clearing screen

--- src/main/java/com/marinavieira/Xadrez/XadrezApplication.java
+++ src/main/java/com/marinavieira/Xadrez/XadrezApplication.java
@@ -1,10 +1,12 @@
 package com.marinavieira.Xadrez;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.marinavieira.chess.ChessException;
 import com.marinavieira.chess.ChessMatch;
 import com.marinavieira.chess.ChessPiece;
 import com.marinavieira.chess.ChessPosition;
@@ -18,18 +20,27 @@ public class XadrezApplication {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 
-
 		while (true) {
-			UI.printBoard(chessMatch.getPieces());
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
 
-			System.out.println();
-			System.out.println("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
 
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			} catch (ChessException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			} catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+
 		}
 
 	}

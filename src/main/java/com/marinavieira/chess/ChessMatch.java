305c54cd214db9f57a4fb2e0a13e31ad7fd2acc7
marigv95 <mama_guima04@hotmail.com>
2023-12-20 12:34:57 -0300

    Queen

--- src/main/java/com/marinavieira/chess/ChessMatch.java
+++ src/main/java/com/marinavieira/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import com.marinavieira.chess.pieces.Bishop;
 import com.marinavieira.chess.pieces.King;
 import com.marinavieira.chess.pieces.Knight;
 import com.marinavieira.chess.pieces.Pawn;
+import com.marinavieira.chess.pieces.Queen;
 import com.marinavieira.chess.pieces.Rook;
 
 import lombok.Data;
@@ -219,6 +220,7 @@ public class ChessMatch {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 		placeNewPiece('e', 1, new King(board, Color.WHITE));
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
@@ -235,6 +237,7 @@ public class ChessMatch {
 		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+		placeNewPiece('d', 8, new Queen(board, Color.BLACK));
 		placeNewPiece('e', 8, new King(board, Color.BLACK));
 		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
